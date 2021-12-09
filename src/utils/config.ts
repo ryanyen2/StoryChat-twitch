dbecbcfa1f5a3068e1abde8501fe8ac945f0c5d6
ryan <ryanyen2-c@my.cityu.edu.hk>
2021-12-09 14:52:41 +0800

    add firebase config

---
+++ src/utils/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: process.env.VUE_APP_FB_API_KEY,
+    authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FB_PROJECT_ID,
+    storageBucket: "storychat-d3e42.appspot.com",
+    messagingSenderId: "424563629842",
+    appId: process.env.VUE_APP_FB_APP_ID,
+    measurementId: process.env.VUE_APP_FB_MEASURE_ID,
+    databaseURL: "https://storychat-d3e42-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
