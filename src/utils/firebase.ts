d92296147eecbb05552bef96ee742ac062795949
ryan <ryanyen2-c@my.cityu.edu.hk>
2021-12-09 15:17:32 +0800

    fb

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,12 +3,21 @@ import { getFirestore } from 'firebase/firestore/lite';
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from 'firebase/database';
 
-import { firebaseConfig } from './config';
+// import { firebaseConfig } from './config';
 import 'firebase/firestore';
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp({
+    apiKey: process.env.VUE_APP_FB_API_KEY,
+    authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FB_PROJECT_ID,
+    storageBucket: "storychat-d3e42.appspot.com",
+    messagingSenderId: "424563629842",
+    appId: process.env.VUE_APP_FB_APP_ID,
+    measurementId: process.env.VUE_APP_FB_MEASURE_ID,
+    databaseURL: "https://storychat-d3e42-default-rtdb.asia-southeast1.firebasedatabase.app/"
+});
 export const analytics = getAnalytics(app);
 export const firestore = getFirestore(app)
 export const db = getDatabase(app);
